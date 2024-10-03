c03fd731c0e858ad4647ba25e72ceaa2672deec8
Gideon <gideonuka78@gmail.com>
2024-10-03 14:31:44 -0700

    nodejs tutorial1

---
+++ example.js
@@ -0,0 +1,16 @@
+//string
+const tech = 'i love tech'
+console.log(tech) // output: i love tech
+
+//number
+const number = 10
+console.log(number) // Output: 10
+
+//boolean
+const boolean = true
+console.log(boolean); // Output: true
+
+//undefined
+const nullVariable = null
+console.log(null); // Output: null
+
